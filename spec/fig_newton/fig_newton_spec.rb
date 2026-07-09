ad9260b8bbb08619db9e0d0eb31b10848a5c526d
Carlos Gutierrez <carlos@spartan-testsolutions.co.uk>
2026-07-09 15:32:31 +0100

    Added unit more unit tests to the fig newton spec

--- spec/fig_newton/fig_newton_spec.rb
+++ spec/fig_newton/fig_newton_spec.rb
@@ -1,14 +1,79 @@
 require 'spec_helper'
 
-describe FigNewton do
-  context 'when asking for data' do
-    it 'should retrieve the data by a key named after the method called' do
-      FigNewton.yml_directory = 'conf'
-      yml_mock = { 'desired_data' => 'information' }
-      expect(File).to receive(:read).with('conf/test').and_return('test')
-      expect(YAML).to receive(:load).and_return(yml_mock)
-      FigNewton.load('test')
-      expect(FigNewton.desired_data).to eql 'information'
+RSpec.describe FigNewton do
+  describe '.default_directory' do
+    it 'returns config/environments' do
+      expect(FigNewton.default_directory).to eq('config/environments')
+    end
+  end
+
+  describe '.yml_directory' do
+    after do
+      FigNewton.yml_directory = nil
+    end
+
+    it 'can be set and read' do
+      FigNewton.yml_directory = 'config/yaml'
+      expect(FigNewton.yml_directory).to eq('config/yaml')
+    end
+  end
+
+  describe '.yml' do
+    it 'can be set and read' do
+      FigNewton.yml = { 'key' => 'value' }
+      expect(FigNewton.yml).to eq('key' => 'value')
+    end
+  end
+
+  describe 'full integration' do
+    before do
+      FigNewton.yml_directory = 'config/yaml'
+    end
+
+    after do
+      FigNewton.yml_directory = nil
+      FigNewton.yml = nil
+    end
+
+    it 'loads a file and retrieves values' do
+      FigNewton.load('test_config.yml')
+      expect(FigNewton.base_url).to eq('http://cheezyworld.com')
+    end
+
+    it 'retrieves integer values' do
+      FigNewton.load('test_config.yml')
+      expect(FigNewton.port).to eq(1234)
+    end
+
+    it 'retrieves true values' do
+      FigNewton.load('test_config.yml')
+      expect(FigNewton.set_flag).to be true
+    end
+
+    it 'retrieves false values' do
+      FigNewton.load('test_config.yml')
+      expect(FigNewton.cleared_flag).to be false
+    end
+
+    it 'retrieves symbol values' do
+      FigNewton.load('test_config.yml')
+      expect(FigNewton.my_symbol).to eq(:hello_world)
+    end
+
+    it 'retrieves float values' do
+      FigNewton.load('test_config.yml')
+      expect(FigNewton.my_float).to eq(0.25)
+    end
+
+    it 'retrieves array values' do
+      FigNewton.load('test_config.yml')
+      expect(FigNewton.my_array).to eq(['one', 2, 3.0, :four])
+    end
+
+    it 'retrieves nested hashes as Nodes' do
+      FigNewton.load('test_config.yml')
+      expect(FigNewton.database).to be_an_instance_of(FigNewton::Node)
+      expect(FigNewton.database.username).to eq('steve')
     end
   end
 end
